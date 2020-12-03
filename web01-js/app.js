225181a16eec23d9b69cc41bbc5f8a51e0dc5275
Adrian <200364@iessierradeguara.com>
2020-12-03 16:43:23 +0100

    Primeros pasos

---
+++ web01-js/app.js
@@ -0,0 +1,20 @@
+document.write("holamundo");
+let texto "Esto es un texto";
+let numero = 24
+let decimal = 5.3;
+let buleano = false;
+let array = ["Manuel", 42, "rubio" ];
+let objeto ={
+    "nombre": "Manuel",
+    "edad": 42,
+    "cabello": "rubio"
+    
+}
+console.log (texto);
+console.log (numero);
+console.log (decimal);
+console.log (buleano);
+console.log (array);
+console.log (objeto);
+console.log (persona.nombre);
+
